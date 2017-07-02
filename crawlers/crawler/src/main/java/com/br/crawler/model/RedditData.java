b796ff7acc2fc2389ce1d2482c129490c1ab0997
Kobra <daniel_yourface@hotmail.com>
2017-07-02 20:58:27 -0300

    [DEV] Desafio Crawler in progress

---
+++ crawlers/crawler/src/main/java/com/br/crawler/model/RedditData.java
@@ -0,0 +1,56 @@
+package com.br.crawler.model;
+
+public class RedditData {
+	
+	public String subReddit;
+	
+	public String titleThread;
+	
+	public String upVotes;
+	
+	public String linkThread;
+	
+	public RedditData(){
+		
+	}
+
+	public RedditData(String subReddit, String titleThread, String upVotes, String linkThread) {
+		this.subReddit = subReddit;
+		this.titleThread = titleThread;
+		this.upVotes = upVotes;
+		this.linkThread = linkThread;
+	}
+
+	public String getSubReddit() {
+		return subReddit;
+	}
+
+	public void setSubReddit(String subReddit) {
+		this.subReddit = subReddit;
+	}
+
+	public String getTitleThread() {
+		return titleThread;
+	}
+
+	public void setTitleThread(String titleThread) {
+		this.titleThread = titleThread;
+	}
+
+	public String getUpVotes() {
+		return upVotes;
+	}
+
+	public void setUpVotes(String upVotes) {
+		this.upVotes = upVotes;
+	}
+
+	public String getLinkThread() {
+		return linkThread;
+	}
+
+	public void setLinkThread(String linkThread) {
+		this.linkThread = linkThread;
+	}
+	
+}
